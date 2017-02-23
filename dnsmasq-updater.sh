2acddf868d2dc53f408e7af16730ed28e84be4c9
Chongwen Huang <iamtaylover@gmail.com>
2017-02-24 03:41:39 +0800

    Update dnsmasq-updater.sh

--- dnsmasq-updater.sh
+++ dnsmasq-updater.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 mkdir /var/log/dnsmasq
+mkdir /etc/dnsmasq.d
 wget https://raw.githubusercontent.com/ManSoRaTech/dnsmasq-conf-for-shadowsocks/master/dnsmasq.conf -O /etc/dnsmasq.conf
 wget https://raw.githubusercontent.com/ManSoRaTech/dnsmasq-conf-for-shadowsocks/master/logrotate.d/dnsmasq -O /etc/logrotate.d/dnsmasq
+wget https://raw.githubusercontent.com/notracking/hosts-blocklists/master/hostnames.txt -O /etc/dnsmasq.d/hostnames.txt
+wget https://raw.githubusercontent.com/notracking/hosts-blocklists/master/domains.txt -O /etc/dnsmasq.d/domains.txt
 service dnsmasq restart
