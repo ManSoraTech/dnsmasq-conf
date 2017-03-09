5cd4a41af57e74e64757f5397e7a33973a6d36ce
Love4Taylor <love4taylor.swift@gmail.com>
2017-03-09 10:19:05 +0800

    Add Adhosts and bilibili live

--- dnsmasq-updater.sh
+++ dnsmasq-updater.sh
@@ -3,6 +3,7 @@ mkdir /var/log/dnsmasq
 mkdir /etc/dnsmasq.d
 wget https://raw.githubusercontent.com/ManSoRaTech/dnsmasq-conf-for-shadowsocks/master/dnsmasq.conf -O /etc/dnsmasq.conf
 wget https://raw.githubusercontent.com/ManSoRaTech/dnsmasq-conf-for-shadowsocks/master/logrotate.d/dnsmasq -O /etc/logrotate.d/dnsmasq
+wget https://raw.githubusercontent.com/ManSoRaTech/dnsmasq-conf-for-shadowsocks/master/adhosts -O /etc/dnsmasq.d/adhosts
 wget https://raw.githubusercontent.com/notracking/hosts-blocklists/master/hostnames.txt -O /etc/dnsmasq.d/hostnames.txt
 wget https://raw.githubusercontent.com/notracking/hosts-blocklists/master/domains.txt -O /etc/dnsmasq.d/domains.txt
 service dnsmasq restart
